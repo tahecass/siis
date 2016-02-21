84763ddbd7cb68b2865c2f909d391f6e0e606ca7
Tatiana Hernandez Casseres <tatiana.hernandez.casseres@gmail.com>
2016-02-21 15:58:17 -0500

    Se adiciona metodo para mostrar notificaciones

--- Siis/src/com/siis/viewModel/framework/Utilidades.java
+++ Siis/src/com/siis/viewModel/framework/Utilidades.java
@@ -5,11 +5,26 @@ import java.util.Map;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Executions;
 
+import fi.jawsy.jawwa.zk.gritter.Gritter;
+
 public class Utilidades {
 
 	public static Component onCargarVentana(Component contenedor, String rutaForma, Map<String, Object> arg)
 			throws Exception {
 
 		return Executions.createComponents(rutaForma, contenedor, arg);
+	}
+
+	public static void mostrarNotificacion(String titulo, String msg, String tipoNotificacion) {
+		String urlImage = "";
+		if (tipoNotificacion.equals("INFO")) {
+			urlImage = "imagenes/informacion.png";
+		} else if (tipoNotificacion.equals("ADVERTENCIA")) {
+			urlImage = "imagenes/advertencia.png";
+		} else if (tipoNotificacion.equals("ERROR")) {
+			urlImage = "imagenes/error.png";
+		}
+		Gritter.notification().withTitle(titulo).withText(msg).withTime(5000).withImage(urlImage).show();
+
 	}
 }
