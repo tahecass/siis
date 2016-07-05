4f9507be150ee9459a5c48cf30ce342f14514817
tahecass <tatiana.hernandez.casseres@gmail.com>
2016-07-05 17:25:19 -0500

    paginacion

---
+++ Siis/src/com/siis/iceberg_sr/contract/IMensajeAction.java
@@ -0,0 +1,29 @@
+package com.siis.iceberg_sr.contract;
+
+public interface IMensajeAction {
+    public static final String MSG_ELIMINAR = "MSG_ELIMINAR";
+    public static final String MSG_SELECCIONE = "MSG_SELECCIONE";
+    public static final String MSN_VALOR_ASUMIDO_VACIO = "MSN_VALOR_ASUMIDO_VACIO";
+    public static final String MSG_SELECCIONE_COLUMNA = "MSG_SELECCIONE_COLUMNA";
+    public static final String MSG_INGRESE_VALOR = "MSG_INGRESE_VALOR";
+    public static final String MSG_SELECCIONE_OPERADOR = "MSG_SELECCIONE_OPERADOR";
+    public static final String MSG_SELECCIONE_CONECTOR = "MSG_SELECCIONE_CONECTOR";
+    public static final String MSG_CONFIGURACION = "MSG_CONFIGURACION";
+    public static final String MSG_CONFIGURACION_PARAM = "MSG_CONFIGURACION_PARAM";
+    public static final String MSG_EJECUTABLE_VIRTUALIZACION_NO = "MSG_EJECUTABLE_VIRTUALIZACION_NO";
+    public static final String MSG_EJECUTABLE_VIRTUALIZACION_SI = "MSG_EJECUTABLE_VIRTUALIZACION_SI";
+    public static final String MSG_LISTA_VACIA = "MSG_LISTA_VACIA";
+    public static final String MSG_ADVERTENCIA_PARAMETRO_SESION = "MSG_ADVERTENCIA_PARAMETRO_SESION";
+    public static final String MSG_NO_EXISTE_TIPO_NOMINA_SESION = "MSG_NO_EXISTE_TIPO_NOMINA_SESION";
+    public static final String MSG_NO_EXISTEN_REGISTROS = "MSG_NO_EXISTEN_REGISTROS";
+    public static final String MSG_ETIQUETA_BUSCAR = "MSG_ETIQUETA_BUSCAR";
+    public static final String MSG_ETIQUETA_REGISTROS = "MSG_ETIQUETA_REGISTROS";
+    public static final String MSG_ETIQUETA_CARGAR_PLANTILLA = "MSG_ETIQUETA_CARGAR_PLANTILLA";
+    public static final String MSG_FORMULARIO_VACIO_PARAMETRIZACION = "MSG_FORMULARIO_VACIO_PARAMETRIZACION";
+    public static final String MSG_INSERCION_OK = "MSG_INSERCION_OK";
+    public static final String MSG_PERSONALIZACION = "MSG_PERSONALIZACION";
+    public static final String MSG_ESTA_SEGURO_BORRAR = "MSG_ESTA_SEGURO_BORRAR";
+    public static final String MSG_BORRAR_REGISTRO = "MSG_BORRAR_REGISTRO";
+    public static final String MSG_BORRAR_OK = "MSG_BORRAR_OK";
+
+}
---
+++ Siis/src/com/siis/iceberg_sr/contract/ISentenciasSQL.java
@@ -0,0 +1,24 @@
+package com.siis.iceberg_sr.contract;
+
+public interface ISentenciasSQL {
+    public static final String SQL_CHAR = "CHAR";
+    public static final String SQL_VARCHAR2 = "VARCHAR2";
+    public static final String SQL_VARCHAR = "VARCHAR";
+    public static final String SQL_BLOB = "BLOB";
+    public static final String SQL_TIMESTAMP = "TIMESTAMP";
+    public static final String SQL_DATE = "DATE";
+    public static final String SQL_NUMBER = "NUMBER";
+    public static final String SQL_OR = "OR";
+    public static final String SQL_AND = "AND";
+    
+    public static final String SQL_BETWEEN = "BETWEEN";
+    public static final String SQL_IN = "IN";
+    public static final String SQL_NOT_IN = "NOT IN";
+    public static final String SQL_SELECT = "SELECT ";
+    public static final String SQL_FROM = " FROM ";
+    public static final String SQL_WHERE = " WHERE ";
+    public static final String SQL_ORDER = " ORDER ";
+    public static final String SQL_BY = " BY ";
+    
+    
+}
